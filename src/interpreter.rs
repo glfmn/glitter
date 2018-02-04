7d9f36c6245d931e2d768ef6faa56c02043e7bf8
Gwen Lofman <Gwen@Lofman.co>
2018-02-04 01:47:25 -0500

    Refactor interpreter to support format expressions
    Format expressions provide the facility to color and format the
    output text with ANSI escape codes in a much more ergonomic and
    readable way.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,27 +1,10 @@
 //! Interpreter which transforms expressions into the desired output
 
+use ansi_term;
+use ansi_term::{Colour, ANSIStrings, ANSIString};
 use git::Stats;
-use ast::{Tree, Expression, Name};
+use ast::{Tree, Expression, Name, Style};
 use std::fmt;
-
-
-/// The interpreter which transforms a gist expression using the provided stats
-#[derive(Debug, PartialEq, Eq, Default, Clone)]
-pub struct Interpreter {
-    stats: Stats,
-}
-
-
-/// Various types of Interpreter errors
-#[derive(PartialEq, Eq, Clone)]
-pub enum InterpreterErr {
-    UnexpectedArgs {
-        exp: Expression,
-    },
-}
-
-
-type InterpretResult = Result<String, InterpreterErr>;
 
 
 /// Trait which determines what is empty in the eyes of the Interpreter
@@ -31,16 +14,69 @@ trait Empty {
     fn is_empty(&self) -> bool;
 }
 
+
 impl Empty for u16 {
     fn is_empty(&self) -> bool { *self == 0 }
 }
+
 
 impl Empty for str {
     fn is_empty(&self) -> bool { self.is_empty() }
 }
 
+
 impl Empty for String {
     fn is_empty(&self) -> bool { self.is_empty() }
+}
+
+
+impl Empty for StyledString {
+    fn is_empty(&self) -> bool { self.result.is_empty() }
+}
+
+
+impl<T> Empty for Vec<T> {
+    fn is_empty(&self) -> bool {self.is_empty()}
+}
+
+
+/// Various types of Interpreter errors
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum InterpreterErr {
+    UnexpectedArgs {
+        exp: Expression,
+    },
+}
+
+
+#[derive(Debug, PartialEq, Clone)]
+struct StyledString {
+    style: ansi_term::Style,
+    result: String,
+}
+
+
+impl<'a> StyledString {
+    fn new(style: ansi_term::Style, result: String) -> StyledString {
+        StyledString {
+            style,
+            result,
+        }
+    }
+
+    fn evaluate(&'a self) -> ANSIString<'a> {
+        self.style.paint(self.result.as_str())
+    }
+}
+
+
+type State = Result<Vec<StyledString>, InterpreterErr>;
+
+
+/// The interpreter which transforms a gist expression using the provided stats
+#[derive(Debug, PartialEq, Eq, Default, Clone)]
+pub struct Interpreter {
+    stats: Stats,
 }
 
 impl Interpreter {
@@ -51,88 +87,82 @@ impl Interpreter {
         }
     }
 
-    ///
-    pub fn evaluate(&self, exps: &Tree) -> InterpretResult {
-        let mut res = String::new();
-        for ref exp in exps.clone().0 {
-            res.push_str(&self.interpret(exp)?);
+    /// Evaluate an expression tree and return the resulting formatted `String`
+    pub fn evaluate(&self, exps: &Tree) -> Result<String, InterpreterErr> {
+        let strings = self.interpret_tree(&exps, ansi_term::Style::new())?;
+        let strings = strings.iter()
+            .map(|s| s.evaluate())
+            .collect::<Vec<ANSIString>>();
+        Ok(format!("{}", ANSIStrings(&strings)))
+    }
+
+    fn interpret_tree(&self, exps: &Tree, context: ansi_term::Style) -> State {
+        let mut res = Vec::new();
+        for e in exps.clone().0 {
+            res.extend(self.interpret(&e, context)?);
         }
         Ok(res)
     }
 
-    fn interpret(&self, exp: &Expression) -> InterpretResult {
+    fn interpret(&self, exp: &Expression, ctx: ansi_term::Style) -> State {
         use ast::Expression::{Named, Group, Literal, Format};
-        use ast::Name::*;
 
         let val = match exp {
-            &Named { ref name, ref sub } => {
-                match name {
-                    &Backslash => self.interpret_literal(sub, "\\")?,
-                    &Quote => self.interpret_literal(sub, "'")?,
-                    &Branch => self.optional_prefix(sub, self.stats.branch.clone(), "")?,
-                    &Remote => self.optional_prefix(sub, self.stats.remote.clone(), "")?,
-                    &Ahead => self.optional_prefix(sub, self.stats.ahead, "+")?,
-                    &Behind => self.optional_prefix(sub, self.stats.behind, "-")?,
-                    &Conflict => self.optional_prefix(sub, self.stats.conflicts, "U")?,
-                    &Added => self.optional_prefix(sub, self.stats.added_staged, "A")?,
-                    &Untracked => self.optional_prefix(sub, self.stats.untracked, "?")?,
-                    &Modified => self.optional_prefix(sub, self.stats.modified_staged, "M")?,
-                    &Unstaged => self.optional_prefix(sub, self.stats.modified, "M")?,
-                    &Deleted => self.optional_prefix(sub, self.stats.deleted, "D")?,
-                    &DeletedStaged => self.optional_prefix(sub, self.stats.deleted_staged, "D")?,
-                    &Renamed => self.optional_prefix(sub, self.stats.renamed, "R")?,
-                    &Stashed => self.optional_prefix(sub, self.stats.stashes, "H")?,
-                }
-            },
-            &Group { ref l, ref r, ref sub } if l == "g(" && r == ")" => {
-                let sub = self.evaluate(sub)?;
-                if sub.is_empty() {
-                    String::new()
-                } else {
-                    format!("{}", sub)
-                }
-            }
+            &Named { ref name, ref sub } => self.interpret_named(name, sub, ctx)?,
             &Group { ref l, ref r, ref sub } => {
-                let sub = self.evaluate(&sub)?;
+                let sub = self.interpret_tree(&sub, ctx)?;
                 if sub.is_empty() {
-                    String::new()
+                    vec![]
                 } else {
-                    format!("{}{}{}",l, sub, r)
+                    let mut res = Vec::with_capacity(sub.len() + 2);
+                    res.push(StyledString::new(ctx, l.to_string()));
+                    res.extend(sub);
+                    res.push(StyledString::new(ctx, r.to_string()));
+                    res
                 }
             },
-            &Literal(ref literal) => literal.to_string(),
-            &Format { style: _, ref sub } => {
-                let sub = self.evaluate(&sub)?;
-                if sub.is_empty() {
-                    String::new()
-                } else {
-                    sub
-                }
-            },
+            &Literal(ref literal) => vec![StyledString::new(ctx, literal.to_string())],
+            &Format { ref style, ref sub } => self.interpret_format(style, sub, ctx)?,
         };
 
         Ok(val)
     }
+
 
     #[inline(always)]
     fn optional_prefix<V1: fmt::Display + Empty, V2: fmt::Display>(&self,
         sub: &Tree,
         val: V1,
         prefix: V2,
-     ) -> InterpretResult {
-        if val.is_empty() { return Ok(String::new()) };
+        ctx: ansi_term::Style,
+    ) -> State {
+        if val.is_empty() { return Ok(Vec::new()) };
         match sub.0.len() {
-            0 => Ok(format!("{}{}", prefix, val)),
+            0 => {
+                Ok(vec![StyledString {
+                    style: ctx,
+                    result: format!("{}{}", prefix, val),
+                }])
+            },
             _ => {
-                Ok(format!("{}{}", self.evaluate(sub)?, val))
+                let mut res = Vec::with_capacity(sub.0.len() + 1);
+                res.extend(self.interpret_tree(sub, ctx)?);
+                res.push(
+                    StyledString {
+                        style: ctx,
+                        result: val.to_string(),
+                    }
+                );
+                Ok(res)
             }
         }
     }
 
+
     #[inline(always)]
-    fn interpret_literal(&self, sub: &Tree, literal: &str) -> InterpretResult {
+    fn interpret_literal(&self, sub: &Tree, literal: &str, context: ansi_term::Style) -> Result<StyledString, InterpreterErr> {
         match sub.0.len() {
-            0 => Ok(literal.to_string()),
+            0 => Ok(StyledString { style: context, result: literal.to_string()}),
             _ => Err(InterpreterErr::UnexpectedArgs {
                 exp: Expression::Named {
                     name: Name::Quote,
@@ -140,6 +170,63 @@ impl Interpreter {
                 },
             }),
         }
+    }
+
+
+    #[inline(always)]
+    fn interpret_named(&self, name: &Name, sub: &Tree, ctx: ansi_term::Style) -> State {
+        use ast::Name::*;
+        match name {
+            &Branch => self.optional_prefix(sub, self.stats.branch.clone(), "", ctx),
+            &Remote => self.optional_prefix(sub, self.stats.remote.clone(), "", ctx),
+            &Ahead => self.optional_prefix(sub, self.stats.ahead, "+", ctx),
+            &Behind => self.optional_prefix(sub, self.stats.behind, "-", ctx),
+            &Conflict => self.optional_prefix(sub, self.stats.conflicts, "U", ctx),
+            &Added => self.optional_prefix(sub, self.stats.added_staged, "A", ctx),
+            &Untracked => self.optional_prefix(sub, self.stats.untracked, "?", ctx),
+            &Modified => self.optional_prefix(sub, self.stats.modified_staged, "M", ctx),
+            &Unstaged => self.optional_prefix(sub, self.stats.modified, "M", ctx),
+            &Deleted => self.optional_prefix(sub, self.stats.deleted, "D", ctx),
+            &DeletedStaged => self.optional_prefix(sub, self.stats.deleted_staged, "D", ctx),
+            &Renamed => self.optional_prefix(sub, self.stats.renamed, "R", ctx),
+            &Stashed => self.optional_prefix(sub, self.stats.stashes, "H", ctx),
+            &Backslash => Ok(vec![self.interpret_literal(sub, "\\", ctx)?]),
+            &Quote => Ok(vec![self.interpret_literal(sub, "'", ctx)?]),
+        }
+    }
+
+    fn interpret_format(&self, style: &Vec<Style>, sub: &Tree, mut context: ansi_term::Style) -> State {
+        use ast::Style::*;
+
+        for s in style {
+            context = match s {
+                &Reset => ansi_term::Style::new(),
+                &Bold => context.bold(),
+                &Underline => context.underline(),
+                &Italic => context.italic(),
+                &FgRed => context.fg(Colour::Red),
+                &BgRed => context.on(Colour::Red),
+                &FgGreen => context.fg(Colour::Green),
+                &BgGreen => context.on(Colour::Green),
+                &FgYellow => context.fg(Colour::Yellow),
+                &BgYellow => context.on(Colour::Yellow),
+                &FgBlue => context.fg(Colour::Blue),
+                &BgBlue => context.on(Colour::Blue),
+                &FgMagenta => context.fg(Colour::Purple),
+                &BgMagenta => context.on(Colour::Purple),
+                &FgCyan => context.fg(Colour::Cyan),
+                &BgCyan => context.on(Colour::Cyan),
+                &FgWhite => context.fg(Colour::White),
+                &BgWhite => context.on(Colour::White),
+                &FgRGB(r,g,b) => context.fg(Colour::RGB(r,g,b)),
+                &BgRGB(r,g,b) => context.on(Colour::RGB(r,g,b)),
+                &FgBlack => context.fg(Colour::Black),
+                &BgBlack => context.on(Colour::Black),
+                &Number(n) => context.fg(Colour::Fixed(n)),
+            };
+        }
+
+        self.interpret_tree(sub, context)
     }
 }
 
