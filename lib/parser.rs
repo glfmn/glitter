734b4210bae1d50458925b00e873f977425866a5
Gwen Lofman <Gwen@Lofman.io>
2019-09-21 14:56:11 -0400

    Experiment with delimited version of take_until
    Works well except that it becomes over-complicated when dealing
    with closing or opening braces inside of strings.  Would need to
    be aware of _all_ delimited pairs and create some sort of token
    tree ala rust in order to function properly.  Has many benefits
    in terms of finding all matching braces, etc, and is worth
    re-visiting if I experiment with custom input types in nom.
    
    The input type could potentially be defined as:
    
    ```
    enum<'a> TokenTree {
        Quote(Vec<TokenTree<'a>>),
        Parens(Vec<TokenTree<'a>>),
        Brace(Vec<TokenTree<'a>>),
        Curly(Vec<TokenTree<'a>>),
        Angle(Vec<TokenTree<'a>>),
        Leaf(&'a str),
    }
    ```
    
    This would add a tokenize stage which builds a Vec of Token Trees
    from the input.

--- lib/parser.rs
+++ lib/parser.rs
@@ -91,10 +91,10 @@ impl<'a> Display for ParseError<'a> {
 
 /// Parse a format
 pub fn parse<'a>(input: &'a str) -> Result<Tree, ParseError<'a>> {
-    use nom::combinator::all_consuming;
+    use nom::combinator::cut;
     use nom::Err;
 
-    all_consuming(expression_tree)(input.as_ref())
+    expression_tree(input.as_ref())
         .map(|(_, tree)| tree)
         .map_err(|e| match e {
             Err::Error(e) => e,
@@ -104,10 +104,10 @@ pub fn parse<'a>(input: &'a str) -> Result<Tree, ParseError<'a>> {
 }
 
 pub fn expression_tree<'a>(input: &'a str) -> IResult<&str, Tree, ParseError<'a>> {
-    use nom::combinator::map;
+    use nom::combinator::{cut, map};
     use nom::multi::many0;
 
-    map(many0(expression), Tree)(input)
+    map(all_consuming_many0(expression), Tree)(input)
 }
 
 /// Parse a single expression, expanding nested expressions
@@ -124,18 +124,46 @@ pub fn expression<'a>(input: &'a str) -> IResult<&str, Expression, ParseError<'a
     ))(input)
 }
 
-fn sub_tree<'a>(input: &'a str) -> IResult<&str, Tree, ParseError<'a>> {
-    use nom::character::complete::char;
-    use nom::combinator::{complete, cut};
-    use nom::sequence::delimited;
+pub fn group_expression<'a>(input: &'a str) -> IResult<&str, Expression, ParseError<'a>> {
+    use nom::branch::alt;
+    use nom::bytes::complete::tag;
+    use nom::combinator::{map, map_parser};
+    use nom::sequence::preceded;
 
-    complete(delimited(
-        char('('),
-        cut(expression_tree),
-        map_err(char(')'), |_, e| {
-            ParseError::missing_delimiter(input, e, ')')
-        }),
+    macro_rules! group {
+        ($lt:expr, $rt:expr, $rc:tt, $d:expr) => {{
+            let contents = map_fail(take_delimited($lt, $rt), |i, e| {
+                ParseError::missing_delimiter(i, e, $rc)
+            });
+
+            let contents = preceded(tag($lt), map_parser(contents, expression_tree));
+
+            map(contents, |sub| Expression::Group { d: $d, sub })
+        }};
+        ($rc:tt, $d:expr) => {{
+            group!($d.left(), $d.right(), $rc, $d)
+        }};
+    }
+
+    alt((
+        group!('>', Delimiter::Angle),
+        group!(']', Delimiter::Square),
+        group!('}', Delimiter::Curly),
+        group!("\\(", ")", ')', Delimiter::Parens),
     ))(input)
+}
+
+fn sub_tree<'a>(input: &'a str) -> IResult<&str, Tree, ParseError<'a>> {
+    use nom::bytes::complete::take_until;
+    use nom::character::complete::char;
+    use nom::combinator::{complete, map_parser};
+    use nom::sequence::preceded;
+
+    let contents = map_fail(take_delimited("(", ")"), |i, e| {
+        ParseError::missing_delimiter(i, e, ')')
+    });
+
+    complete(preceded(char('('), map_parser(contents, expression_tree)))(input)
 }
 
 pub fn named_expression<'a>(input: &'a str) -> IResult<&str, Expression, ParseError<'a>> {
@@ -292,34 +320,6 @@ pub fn format_expression<'a>(input: &'a str) -> IResult<&str, Expression, ParseE
     })
 }
 
-pub fn group_expression<'a>(input: &'a str) -> IResult<&str, Expression, ParseError<'a>> {
-    use nom::branch::alt;
-    use nom::bytes::complete::tag;
-    use nom::character::complete::char;
-    use nom::combinator::{complete, map};
-    use nom::sequence::delimited;
-
-    macro_rules! group {
-        ($l:tt, $r:tt, $type:expr) => {
-            map(
-                complete(delimited(
-                    tag($l),
-                    expression_tree,
-                    map_fail(char($r), |_, e| ParseError::char_to_delimiter(input, e)),
-                )),
-                |sub| Expression::Group { d: $type, sub },
-            )
-        };
-    }
-
-    alt((
-        group!("<", '>', Delimiter::Angle),
-        group!("[", ']', Delimiter::Square),
-        group!("{", '}', Delimiter::Curly),
-        group!("\\(", ')', Delimiter::Parens),
-    ))(input)
-}
-
 pub fn literal_expression<'a>(input: &'a str) -> IResult<&str, Expression, ParseError<'a>> {
     use nom::bytes::complete::take_until;
     use nom::character::complete::char;
@@ -368,6 +368,87 @@ pub fn separator_expression<'a>(input: &'a str) -> IResult<&str, Expression, Par
         )),
         Expression::Separator,
     )(input)
+}
+
+pub fn take_delimited<T, Input, Error: error::ParseError<Input>>(
+    tag_l: T,
+    tag_r: T,
+) -> impl Fn(Input) -> IResult<Input, Input, Error>
+where
+    Input: nom::InputTake + nom::FindSubstring<T> + Clone + std::fmt::Display + nom::InputLength,
+    T: nom::InputLength + Clone + std::fmt::Display,
+{
+    use nom::error::ErrorKind::TakeUntil;
+    use nom::Err;
+    move |i: Input| {
+        let l = tag_l.clone();
+        let r = tag_r.clone();
+        let l_len = l.input_len();
+        let r_len = r.input_len();
+        eprintln!("\n\"{}\",\"{}\" delimit input {}", l, r, i);
+
+        // Count open delimiters while searching for the close
+        let mut search = i.clone();
+        let mut counter = 1;
+        let mut offset = 0;
+        while counter > 0 {
+            let right = match search.find_substring(r.clone()) {
+                None => return Err(Err::Error(Error::from_error_kind(i, TakeUntil))),
+                Some(index) => index,
+            };
+
+            let left = search.find_substring(l.clone()).unwrap_or(right);
+
+            if left < right {
+                offset += left + l_len;
+                counter += 1;
+                search = search.take_split(left + l_len).0
+            } else {
+                offset += right + r_len;
+                counter -= 1;
+                search = search.take_split(right + r_len).0
+            }
+        }
+
+        let (rest, delimited) = i.take_split(offset - r_len);
+        let rest = rest.take_split(r_len).0;
+
+        Ok((rest, delimited))
+    }
+}
+
+pub fn all_consuming_many0<I, O, E, F>(f: F) -> impl Fn(I) -> IResult<I, Vec<O>, E>
+where
+    I: Clone + PartialEq + nom::InputLength,
+    F: Fn(I) -> IResult<I, O, E>,
+    E: error::ParseError<I>,
+{
+    use error::ErrorKind::Many0;
+    use nom::Err;
+    move |i: I| {
+        let mut acc = Vec::with_capacity(4);
+        let mut i = i.clone();
+        loop {
+            match f(i.clone()) {
+                Err(Err::Error(e)) => {
+                    if i.clone().input_len() == 0 {
+                        return Ok((i, acc));
+                    } else {
+                        return Err(Err::Failure(e));
+                    }
+                }
+                Err(e) => return Err(e),
+                Ok((i1, o)) => {
+                    if i1 == i {
+                        return Err(Err::Error(E::from_error_kind(i, Many0)));
+                    }
+
+                    i = i1;
+                    acc.push(o);
+                }
+            }
+        }
+    }
 }
 
 /// Apply a function to the error returned by a parser
